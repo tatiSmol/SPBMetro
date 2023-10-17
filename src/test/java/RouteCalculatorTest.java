ac857eb2157b83155c98113ae0be1a2a30e39e22
tatiSmol <judeclapton@icloud.com>
2023-10-17 14:39:23 +0300

    Written unit tests for all methods of the RouteCalculator class

--- src/test/java/RouteCalculatorTest.java
+++ src/test/java/RouteCalculatorTest.java
@@ -2,28 +2,74 @@ import core.Line;
 import core.Station;
 import junit.framework.TestCase;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RouteCalculatorTest extends TestCase {
-    List<Station> route;
+    List<Station> firstR, secondR, thirdR;
+    StationIndex stationIndex;
+    RouteCalculator routeCalculator;
+    Station alpha, beta, gamma, delta, epsilon, zeta;
 
     @Override
     protected void setUp() throws Exception {
-        route = new ArrayList<>();
+        Line l1 = new Line(1, "L1");
+        Line l2 = new Line(2, "L2");
+        Line l3 = new Line(3, "L3");
 
-        Line line1 = new Line(1, "First");
-        route.add(new Station("Alpha", line1));
-        route.add(new Station("Beta", line1));
+        stationIndex = new StationIndex();
+        Stream.of(l1, l2, l3).forEach(stationIndex::addLine);
 
-        Line line2 = new Line(2, "Second");
-        route.add(new Station("Gamma", line2));
-        route.add(new Station("Delta", line2));
+        alpha = new Station("Alpha", l1);
+        beta = new Station("Beta", l1);
+        gamma = new Station("Gamma", l1);
+        delta = new Station("Delta", l2);
+        epsilon = new Station("Epsilon", l2);
+        zeta = new Station("Zeta", l3);
+
+        Stream.of(alpha, beta, gamma, delta, epsilon, zeta)
+                .peek(station -> station.getLine().addStation(station)).
+                forEach(stationIndex::addStation);
+
+
+        stationIndex.addConnection(Stream.of(beta, epsilon).collect(Collectors.toList()));
+        stationIndex.addConnection(Stream.of(delta, zeta).collect(Collectors.toList()));
+
+        stationIndex.getConnectedStations(beta);
+        stationIndex.getConnectedStations(delta);
+
+        routeCalculator = new RouteCalculator(stationIndex);
+
+        firstR = Stream.of(alpha, beta, gamma).collect(Collectors.toList());
+        secondR = Stream.of(alpha, beta, epsilon).collect(Collectors.toList());
+        thirdR = Stream.of(beta, epsilon, delta, zeta).collect(Collectors.toList());
+    }
+
+        /*
+        L1          L2              L3
+        alpha       delta     >>>   zeta
+        beta   >>>  epsilon
+        gamma
+        */
+
+    public void testGetShortestRoute() {
+        List<Station> noTransfers = routeCalculator.getShortestRoute(alpha, gamma);
+        List<Station> shortestWay = firstR;
+        assertEquals(noTransfers, shortestWay);
+
+        List<Station> oneTransfer = routeCalculator.getShortestRoute(alpha, epsilon);
+        List<Station> littleLongerWay = secondR;
+        assertEquals(oneTransfer, littleLongerWay);
+
+        List<Station> twoTransfers = routeCalculator.getShortestRoute(beta, zeta);
+        List<Station> longestWay = thirdR;
+        assertEquals(twoTransfers, longestWay);
     }
 
     public void testCalculateDuration() {
-        double actual = RouteCalculator.calculateDuration(route);
-        double expected = 8.5;
+        double actual = RouteCalculator.calculateDuration(thirdR);
+        double expected = 9.5;
 
         assertEquals(expected, actual);
     }
